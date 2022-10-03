dd7d87b7c21801e4531ce84daa64d86aeac004df
Basith P <basithpofficial@gmail.com>
2022-10-03 20:27:22 +0530

    Count down functionality

--- main.py
+++ main.py
@@ -12,6 +12,24 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
+# --------------- TIMER MECHANISM ---------------- #
+
+
+def start_timer():
+    count_down(WORK_MIN * 60)
+
+
+# --------------- COUNTDOWN MECHANISM ---------------- #
+
+def count_down(count):
+    count_min = count // 60
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # --------------- UI SETUP ---------------- #
 
@@ -27,12 +45,11 @@ title.grid(column=1, row=0)
 canvas = Canvas(width=300, height=150, highlightthickness=0)
 bg = PhotoImage(file="bg.png")
 canvas.create_image(150, 75, image=bg)
-canvas.create_text(150, 70, text="00:00", fill="white",
-                   font=(FONT_NAME, 50, "bold")
-                   )
+timer_text = canvas.create_text(150, 70, text="00:00", fill="white",
+                                font=(FONT_NAME, 50, "bold"))
 canvas.grid(column=1, row=1)
 
-start_button = Button(text="Start", highlightthickness=0)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset", highlightthickness=0)
